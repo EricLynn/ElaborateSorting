2b6ce31609874f3fcda381a7eb9dc2c13025d882
EricLynn <32644694+EricLynn@users.noreply.github.com>
2018-10-10 14:44:37 -0400

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,42 @@
+public class InsertionSort{
+
+    public static int[] sort(int[] items){
+	int comparisons, swaps;
+	swaps = comparisons = 0;
+
+	for(int i = 1; i<items.length; i++){
+	    int[] sortedItems = new int[i];
+	    for(int j = 0; j<sortedItems.length; j++){
+		sortedItems[j] = items[j];
+	    }
+	    int low = 0;
+	    int high = sortedItems.length-1;
+	    int mid = 0;
+	    while(high>low){
+		mid = (high + low)/2;
+		comparisons++;
+		if(items[i]>items[mid]){
+		    low = mid+1;
+		}
+		else
+		    high = mid;
+	    }
+	    comparisons++;
+	    int index = 0;
+	    if(items[i]<items[mid])
+		index = mid;
+	    else
+		index = mid+1;
+
+	    int temp = items[i];
+	    for(int j = i-1; j>=index; j--){
+		swaps++;
+		items[j+1] = items[j];
+	    }
+	    items[index] = temp;
+	    
+	}
+
+    return items;
+
+}
